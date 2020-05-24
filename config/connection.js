26334e86875680f7ff68e61ef6debc82545ce2c7
Georgie <georginakellis@gmail.com>
2020-05-24 17:37:50 +0800

    attempts to fix heroku errors

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Northstead136",
-    database: "burgers_db"
+let connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Northstead136",
+  database: "burgers_db"
 });
 
 
